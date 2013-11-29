2ed5643acc1904b00d8275c753655e9d47094f37
Tim <nighttrain0486@yahoo.com>
2013-11-29 18:17:13 -0500

    Chapter 4 text analyzer

--- analyzer.rb
+++ analyzer.rb
@@ -1,25 +1,40 @@
-lines = File.readlines("text.txt")
+stopwords = %w{the a by on for of are with just but and to the my I has some in}
+lines = File.readlines(ARGV[0])
 line_count = lines.size
 text = lines.join
 
-puts "#{line_count} lines"
+# Count the characters
+character_count = text.length
+character_count_nospaces = text.gsub(/\s+/, '').length
 
-total_characters = text.length
-puts "#{total_characters} characters"
-
-total_characters_nospaces = text.gsub(/\s+/, '').length
-puts "#{total_characters_nospaces} characters excluding spaces"
-
+# Count the words, sentences, and paragraphs
 word_count = text.split.length
-puts "#{word_count} words"
-
-sentence_count = text.split(/\.|\?|\!/).length
-puts "#{sentence_count} sentences."
-
+sentence_count = text.split(/\.|\?|!/).length
 paragraph_count = text.split(/\n\n/).length
-puts "#{paragraph_count} paragraphs."
 
+# Make a list of words that are not stop words,
+# count them, and work out the percentage of non-stop words
+# against all words
+all_words = text.scan(/\w+/)
+good_words = all_words.select{ |word| !stopwords.include?(word) }
+good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
+
+# Summarize text by cherry picking some choice sentences
+sentences = text.gsub(/\s+/, ' ').strip.split(/\.|\?|!/)
+sentences_sorted = sentences.sort_by { |sentence| sentence.length}
+one_third = sentences_sorted.length / 3
+ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
+ideal_sentences = ideal_sentences.select { |sentence| sentence =~ /is|are/ }
+
+# Give the analysis back to the user
+puts "#{line_count} lines"
+puts "#{character_count} characters"
+puts "#{character_count_nospaces} characters (excluding spaces)"
+puts "#{word_count} words"
+puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
-
-stopwords = %w{the a by on for of are with just but and to the my I has some in}
+puts "#{good_percentage}% of words are non fluff words"
+puts "Summary:\n\n" + ideal_sentences.join(". ")
+puts "-- End of analysis"
